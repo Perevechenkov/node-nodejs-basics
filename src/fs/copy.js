e70ffdbf76388d61a1a62b7f87c99090d27e2b77
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 17:26:39 +0500

    feat(fs): copy directory

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { cp } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const srcDirPath = path.join(__dirname, 'files');
+    const destDirPath = path.join(__dirname, 'files_copy');
+
+    try {
+        await cp(srcDirPath, destDirPath, {
+            errorOnExist: true,
+            force: false,
+            recursive: true,
+        });
+    } catch (err) {
+        console.log(err.message);
+        if (['ERR_FS_CP_EEXIST', 'ENOENT'].includes(err.code))
+            throw new Error('FS operation failed');
+    }
 };
 
 copy();
