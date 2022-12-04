dbb016452398c822e5612b72a8077935a79f6d08
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 16:51:31 +0500

    feat(fs): create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    try {
+        await fs.appendFile(filePath, 'I am fresh and young', { flag: 'ax' });
+        console.log('new file created');
+    } catch (err) {
+        console.log(err.message);
+        if (err.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await create();
