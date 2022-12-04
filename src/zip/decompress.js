ee8eb5926b66d322f8481e1123e798504d23fd30
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-05 00:29:57 +0500

    feat(zip): decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+import getDir from '../getDir.js';
+
+const { __dirname } = getDir(import.meta.url);
+const pipe = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+    const srcFilePath = path.join(__dirname, 'files', 'archive.gz');
+    const destFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+    const src = createReadStream(srcFilePath);
+    const dest = createWriteStream(destFilePath);
+    const gzip = createUnzip();
+
+    try {
+        await pipe(src, gzip, dest);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 await decompress();
