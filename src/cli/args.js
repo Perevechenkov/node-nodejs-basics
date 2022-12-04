ad82ea6120eb256c6cbcb8f34cca8542cd950934
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 19:21:42 +0500

    feat(cli): print cmd arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    for (let i = 2; i < args.length; i++) {
+        if (/^--/.test(args[i])) {
+            console.log(`${args[i]} is ${args[i + 1]}`);
+        }
+    }
 };
 
 parseArgs();
