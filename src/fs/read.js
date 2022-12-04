517301225d59c6bcf190a2f7c90c39b5ca2aab17
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 18:54:24 +0500

    feat(fs): read file content

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const targetFilePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const content = await readFile(targetFilePath, { encoding: 'utf8' });
+        console.log(content);
+    } catch (err) {
+        console.log(err.message);
+        if (err.code === 'ENOENT') throw new Error('FS operation failed');
+    }
 };
 
 await read();
