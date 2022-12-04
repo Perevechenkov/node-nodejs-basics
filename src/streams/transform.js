0e95276021c148e5983dad4bf3c1c823ae134614
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 23:35:04 +0500

    feat(stream): transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const myTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkStr = chunk.toString().trim();
+            const reversedChunk = chunkStr.split('').reverse().join('');
+
+            this.end();
+            callback(null, reversedChunk + '\n');
+        },
+    });
+
+    myTransform.on('finish', () => {
+        process.exit();
+    });
+
+    pipeline(process.stdin, myTransform, process.stdout, err => {
+        console.log(err);
+        process.exit();
+    });
 };
 
 await transform();
