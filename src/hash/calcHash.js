e6aa4b3dbb664f565f68aa614566959bbb77afef
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 20:16:54 +0500

    feat(hash): print file hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const targetDirPath = path.join(
+        __dirname,
+        'files',
+        'fileToCalculateHashFor.txt'
+    );
+
+    const content = await readFile(targetDirPath);
+
+    console.log(createHash('SHA256').update(content).digest('hex'));
 };
 
 await calculateHash();
