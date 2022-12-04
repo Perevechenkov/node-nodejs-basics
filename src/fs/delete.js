3c491779589976389ac0f5ebceb52fec7272f70b
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 18:46:35 +0500

    feat(fs): delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { unlink } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const targetFilePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await unlink(targetFilePath);
+    } catch (err) {
+        console.log(err.message);
+        if (err.code === 'ENOENT') throw new Error('FS operation failed');
+    }
 };
 
 await remove();
