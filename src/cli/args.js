464b839cd970d06571a9cd0f7b370e056199c937
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-05 01:08:45 +0500

    fix(cli): remove '--' from argument name and format output

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,10 +1,14 @@
 const parseArgs = () => {
     const args = process.argv;
+    const formattedArgs = [];
     for (let i = 2; i < args.length; i++) {
         if (/^--/.test(args[i])) {
-            console.log(`${args[i]} is ${args[i + 1]}`);
+            formattedArgs.push(
+                `${args[i].replace('--', '')} is ${args[i + 1]}`
+            );
         }
     }
+    console.log(formattedArgs.join(', '));
 };
 
 parseArgs();
