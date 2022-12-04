0c9a79a6613785664d85bf4c7523f2484b5c775f
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 21:02:00 +0500

    feat(streams): write data from stdin to file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const targetFilePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const ws = createWriteStream(targetFilePath);
+
+    ws.on('error', err => {
+        console.log(err);
+        process.exit();
+    });
+
+    ws.on('finish', () => {
+        console.log('Finished');
+        process.exit();
+    });
+
+    process.stdin.on('data', data => {
+        ws.write(data);
+        ws.end();
+    });
 };
 
 await write();
