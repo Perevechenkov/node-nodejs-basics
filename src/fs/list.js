bb19714051ba50c2e775de0a5ac3d320cf1858a1
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 18:50:53 +0500

    feat(fs): list files from directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { readdir } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const targetDirPath = path.join(__dirname, 'files');
+
+    try {
+        const files = await readdir(targetDirPath);
+
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (err) {
+        console.log(err.message);
+        if (err.code === 'ENOENT') throw new Error('FS operation failed');
+    }
 };
 
 await list();
