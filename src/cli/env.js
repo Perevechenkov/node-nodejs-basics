a01e3889acbf141ed80907144cf3c2f7f3ca138f
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 19:13:22 +0500

    feat(cli): print env variables with 'RSS_' prefix

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (const variable in process.env) {
+        if (/^RSS_/.test(variable)) {
+            console.log(`${variable}=${process.env[variable]}`);
+        }
+    }
 };
 
 parseEnv();
