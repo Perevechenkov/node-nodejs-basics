a667b7ae6e9a65cd952b9cd0d112cdfcb735e709
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-04 18:40:16 +0500

    feat(fs): rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { existsSync } from 'node:fs';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const targetFilePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const newFilePath = path.join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        if (existsSync(newFilePath)) throw { code: 'EEXIST', message: `${newFilePath} already exists` };
+        await fs.rename(targetFilePath, newFilePath);
+    } catch (err) {
+        console.log(err.message);
+        if (['EEXIST', 'ENOENT'].includes(err.code))
+            throw new Error('FS operation failed');
+    }
 };
 
 await rename();
