e34cbe58a711787d9cf5f59fbd59e492efa77ced
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-06 00:28:14 +0500

    feat(cp): completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,28 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'node:child_process';
+import path from 'node:path';
+import getDir from '../getDir.js';
+
+const { __dirname } = getDir(import.meta.url);
+
+const spawnChildProcess = async args => {
+    const childScriptPath = path.join(__dirname, 'files', 'script.js');
+
+    const cp = fork(childScriptPath, args);
+
+    cp.on('spawn', () => {
+        console.log('Successfully spawned');
+    });
+
+    cp.on('error', err => {
+        console.error(err.message);
+    });
+
+    cp.on('close', code => {
+        if (code !== 0) {
+            throw new Error(`err code ${code}`);
+        }
+        console.log(`Child process exited. Code: ${code}`);
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess(['arg1', 'arg2']);
